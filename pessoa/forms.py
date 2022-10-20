e4d92c457354b3da838e61789d398c2c0e999d14
Pedro Andriotti <phab@cesar.school>
2022-10-20 09:05:49 -0300

    Adição de Forms

---
+++ pessoa/forms.py
@@ -0,0 +1,9 @@
+from socket import fromshare
+from django import forms
+from .models import Pessoa
+
+class PessoaForm(forms.ModelForm):
+    class Meta:
+        model = Pessoa
+        fields = ['nome_completo', 'data_nascimento', 'ativa']
+        
---
+++ pessoa/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Pessoa(models.Model):
+    nome_completo = models.CharField(max_length=256)
+    data_nascimento = models.DateField(null=True)
+    ativa = models.BooleanField(default=True)
+
+    def __str__(self) -> str:
+        return self.nome_completo
